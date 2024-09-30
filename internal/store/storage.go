043d16a349afb5c128c9cb53241db7a01dbfface
Mohamed Abdelazem <mabdelazemahmed@gmail.com>
2024-09-30 16:13:05 +0300

    adding comments

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -5,20 +5,25 @@ import (
 	"database/sql"
 )
 
+// Posts interface
 type Posts interface {
 	Create(context.Context, *Post) error
 }
 
+// Users interface
 type Users interface {
 	Create(context.Context, *User) error
 	GetUsers(context.Context) ([]User, error)
 }
 
+// Storage Store
 type Storage struct {
 	Posts Posts
 	Users Users
 }
 
+// NewStorage creates a new storage
+// and returns a pointer to it
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
 		Posts: &PostsStore{db: db},
